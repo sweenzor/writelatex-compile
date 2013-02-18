7ddc5efae57647a2e2bcdb20e28e44451633a9fc
Matt Sweeney <mattswe@gmail.com>
2013-02-17 20:52:45 -0800

    Save resulting PDF to disk

--- compile.py
+++ compile.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python
 
-import requests
+import sys, time
 import json
+import requests
 
-fid = open('example.tex', 'rb')
+
+# Read arugments
+try:
+    filename = sys.argv[1]
+except IndexError:
+    print 'looking for: \'python compile.py example.tex\''
+    quit()
+
+# Load contents of .tex file
+fid = open(filename, 'rb')
 text = fid.read()
+fid.close()
 
+# Post contents to writeLaTeX
 payload = {'snip': text, 'spash': 'none'}
 r = requests.post('https://www.writelatex.com/docs', data=payload)
 
-print json.dumps(payload)
+# Recieve response
 print r.url
+doc_id = r.url.split('/')[3]
 
+# Get address of PDF
+time.sleep(4)
+r = requests.post('https://www.writelatex.com/docs/%s/pdf' % doc_id)
+pdf_url = 'http://%s' % r.text.split('\'')[1]
+r = requests.get(pdf_url)
+print 'PDF size: %s bytes' % len(r.content)
 
+# Write PDF to file
+pdf_filename = '%s.pdf' % filename.split('.tex')[0]
+fid = open(pdf_filename, 'wb')
+fid.write(r.content)
+fid.close()
+print 'PDF saved: \'%s\'' % pdf_filename
